4405291b49b3f7f19a550f2a7f338ba8801618f9
Benjamin Altpeter <benjamin@oldpeter.de>
2017-03-16 23:55:51 +0100

    Overwrote the logger

--- lib/acme/Core/Application.php
+++ lib/acme/Core/Application.php
@@ -3,6 +3,8 @@
 namespace acme\Core;
 
 use acme\DataIo\DatabaseConnection;
+use Monolog\Handler\BrowserConsoleHandler;
+use Monolog\Logger;
 
 class Application extends \boilerplate\Core\Application {
     const VERSION_TEXT = '1.0.0';
@@ -12,5 +14,7 @@ class Application extends \boilerplate\Core\Application {
         parent::__construct();
         $this->db_con = new DatabaseConnection();
         $this->config = new Configuration(true, $this->db_con);
+        $this->logger = new Logger('default');
+        $this->logger->pushHandler(new BrowserConsoleHandler());
     }
 }
