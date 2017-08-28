b8fccfd93d4888bbe3cc86b165a2b94e6e3ccc71
Benjamin Altpeter <benjamin@oldpeter.de>
2017-08-29 01:51:02 +0200

    Added examples for controllers in routes

--- routes.php
+++ routes.php
@@ -1,17 +1,22 @@
 <?php
 require_once __DIR__ . '/lib/base.php';
 
-// Use this file to configure all your routes
+// Use this file to configure all your routes. Here are some examples of how you can configure routes.
 
 \boilerplate\Core\Router::get('/', function() {
     return 'Welcome to Boilerplate!';
 }, 'home');
 
-\boilerplate\Core\Router::get('hello/{name}', function($name) {
-    return 'Hi there, ' . $name . '!
-    <br>Say <a href="' . route('bye', array('name' => $name)) . '">bye</a>.';
-});
+\boilerplate\Core\Router::get('hello/{name}', 'helloAction');
 
 \boilerplate\Core\Router::get('bye/{name}', function($name) {
     return view('demo/bye.html', array('name' => $name));
 }, 'bye');
+
+\boilerplate\Core\Router::setControllerNamespace('\\acme\\Controller\\');
+\boilerplate\Core\Router::get('/controller/{input}', 'DemoController@demoAction');
+
+function helloAction($name) {
+    return 'Hi there, ' . $name . '!
+<br>Say <a href="' . route('bye', array('name' => $name)) . '">bye</a>.';
+}
