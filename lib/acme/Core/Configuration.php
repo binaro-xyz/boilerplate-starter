74b73775c2472d9994308c93644e20ff428fa533
Benjamin Altpeter <benjamin@oldpeter.de>
2017-09-03 00:38:27 +0200

    Moved ConfigurationOption to its own file

--- lib/acme/Core/Configuration.php
+++ lib/acme/Core/Configuration.php
@@ -2,12 +2,6 @@
 
 namespace acme\Core;
 
-class ConfigurationOption extends \boilerplate\Core\ConfigurationOption {
-    const DEMO_INI_PROPERTY = array('property' => 'demo_ini_property', 'source' => 'ini');
-
-    const DEMO_DB_PROPERTY = array('property' => 'demo_db_property', 'source' => 'db');
-}
-
 class Configuration extends \boilerplate\Core\Configuration {
 
 }
---
+++ lib/acme/Utility/ConfigurationOption.php
@@ -0,0 +1,7 @@
+<?php namespace acme\Utility;
+
+class ConfigurationOption extends \boilerplate\Utility\ConfigurationOption {
+    const DEMO_INI_PROPERTY = array('property' => 'demo_ini_property', 'source' => 'ini');
+
+    const DEMO_DB_PROPERTY = array('property' => 'demo_db_property', 'source' => 'db');
+}
